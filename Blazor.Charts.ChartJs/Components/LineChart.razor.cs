69291cb400344265e07334ffe41f75b4fae09f7f
rdw100 <ryandustinwright@hotmail.com>
2021-03-16 13:53:44 -0400

    Added multiple configurable properties via anonymous type.

--- Blazor.Charts.ChartJs/Components/LineChart.razor.cs
+++ Blazor.Charts.ChartJs/Components/LineChart.razor.cs
@@ -40,7 +40,45 @@ namespace Blazor.Charts.ChartJs.Components
         public string Title { get; set; }
 
         [Parameter]
-        public string DatasetTitle { get; set; } = "My First Dataset";
+        public string DatasetTitle { get; set; } = "dataset";
+
+        readonly DataItem[] myData = new DataItem[] {
+            new DataItem
+            {
+                X = 2.75,
+                Y = 50
+            },
+            new DataItem
+            {
+                X = 5.5,
+                Y = 60
+            },
+            new DataItem
+            {
+                X = 7,
+                Y = 70
+            },
+            new DataItem
+            {
+                X = 9,
+                Y = 80
+            },
+            new DataItem
+            {
+                X = 11,
+                Y = 90
+            },
+            new DataItem
+            {
+                X = 12.5,
+                Y = 100
+            },
+            new DataItem
+            {
+                X = 15,
+                Y = 110
+            }
+        };
 
         /// <summary>
         /// Instantiates the Chart class with all configurable options.
@@ -55,6 +93,7 @@ namespace Blazor.Charts.ChartJs.Components
         /// are useful for performing interop, or interacting with values received from <c>@ref</c>.
         /// Use the <paramref name="firstRender" /> parameter to ensure that initialization work is only performed
         /// once.
+        /// https://www.chartjs.org/docs/latest/axes/cartesian/linear.html#linear-cartesian-axis
         /// </remarks>
         protected override async Task OnAfterRenderAsync(bool firstRender)
         {
@@ -64,19 +103,39 @@ namespace Blazor.Charts.ChartJs.Components
                 Options = new
                 {
                     Responsive = true,
-                    //Title = new
-                    //{
-                    //    Display = true,
-                    //    Text = Title
-                    //},
+                    Title = new
+                    {
+                        Display = true,
+                        Text = "Birth to 5 Years (Percentile)"
+                    },
                     Scales = new
                     {
                         YAxes = new[]
                         {
-                        new { Ticks = new {
-                            BeginAtZero=true
-                        } }
-                    }
+                            new {
+                                ScaleLabel = new { Display = true, labelString = "Weight (kg)" },
+                                Ticks = new { BeginAtZero = true, StepSize = 2, SuggestedMax = 26, SuggestMin = 0 }
+                            }
+                        },
+                        XAxes = new[]
+                        {
+                            new {
+                                ScaleLabel = new { Display = true, labelString = "Length (cm)" }
+                            }
+                        }
+                    },
+                    Legend = new
+                    {
+                        Display = true,
+                        Position = "right",
+                        Labels = new
+                        {
+                            fontColor = "rgb(255, 99, 132)"
+                        }
+                    },
+                    Series = new[]
+                    {
+                        new {Color = "#e10808" }
                     }
                 },
                 Data = new
@@ -84,11 +143,12 @@ namespace Blazor.Charts.ChartJs.Components
                     Datasets = new[]
                     {
                     new {
-                            Data = Data,
-                            BackgroundColor = BackgroundColor,
+                            Data = myData, //Data
+                            //BackgroundColor = BackgroundColor,
                             BorderColor = BorderColor,
                             BorderWidth = BorderWidthNum,
-                            Label = DatasetTitle
+                            Label = DatasetTitle,
+                            fill = false
                     }
                 },
                     Labels = Labels
@@ -98,5 +158,11 @@ namespace Blazor.Charts.ChartJs.Components
             // Inject the IJSRuntime abstraction into a class (.cs)
             await JSRuntime.InvokeVoidAsync("setup", Id, config);
         }
+    }
+
+    class DataItem
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
     }
 }
