62fa2566fed91fec5dc5d7c8b19bf90bcce0812f
rdw100 <ryandustinwright@hotmail.com>
2021-03-21 12:21:55 -0400

    Added project to further demonstrate Blazor JSInterop with ChartJs.  Loads a Typed List into Data parameter in JSInterOp call to the Draw method.

---
+++ Blazor.Charts.ChartJs.JsInterOp/Data/Hierarchy.cs
@@ -0,0 +1,9 @@
+﻿namespace Blazor.Charts.ChartJs.JsInterOp.Data
+{
+    public class Hierarchy
+    {
+        public int MemberId { get; set; }
+        public string Name { get; set; }
+        public int? ParentId { get; set; }
+    }
+}
