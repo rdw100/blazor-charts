722e0d964c93c121be3dc9b24ffc803d4e23d1b6
rdw100 <ryandustinwright@hotmail.com>
2021-03-23 17:13:24 -0400

    Added Pie and Line charts to same Blazor page using same data.   Javascript literal object is passed into the data table constructor.

--- Blazor.Charts.ChartJs.JsInterOp/Data/pChart.cs
+++ Blazor.Charts.ChartJs.JsInterOp/Data/pChart.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Text.Json;
+//using System.Data;
 
 namespace Blazor.Charts.ChartJs.JsInterOp.Data
 {
@@ -27,26 +24,165 @@ namespace Blazor.Charts.ChartJs.JsInterOp.Data
                 //return DBHelper.ConvertToDataTable<pData>(data);
             }
         }
+
+        //    ['Year', 'Sales', 'Expenses'],
+        //    ['2004', 1000, 400],
+        //    ['2005', 1170, 460],
+        //    ['2006', 660, 1120],
+        //    ['2007', 1030, 540]
+        //    [{ label: 'Year', id: 'year' },
+        //    { label: 'Sales', id: 'Sales', type: 'number' }, // Use object notation to explicitly specify the data type.
+        //    { label: 'Expenses', id: 'Expenses', type: 'number' }],
+        //    ['2014', 1000, 400],
+        //    ['2015', 1170, 460],
+        //    ['2016', 660, 1120],
+        //    ['2017', 1030, 540]]);
+
+        //['Task', 'Hours per Day'],
+        //        ['Work', 11],
+        //        ['Eat', 2],
+        //        ['Commute', 2],
+        //        ['Watch TV', 2],
+        //        ['Sleep', 7]
+
+        public byte[] OnGetChartData()
+        {
+            var chart = new Chart
+            {
+                cols = new object[]
+                {
+                    new { id = "topping", type = "string", label = "Topping" },
+                    new { id = "slices", type = "number", label = "Slices" }
+                },
+                rows = new object[]
+                {
+                    new { c = new object[] { new { v = "Mushrooms" }, new { v = 3 } } },
+                    new { c = new object[] { new { v = "Onions" }, new { v = 1 } } },
+                    new { c = new object[] { new { v = "Olives" }, new { v = 1 } } },
+                    new { c = new object[] { new { v = "Zucchini" }, new { v = 1 } } },
+                    new { c = new object[] { new { v = "Pepperoni" }, new { v = 2 } } }
+                }
+            };
+
+
+            byte[] jsonString = JsonSerializer.SerializeToUtf8Bytes(chart);
+
+            return jsonString;
+
+            //var pizza = new[]
+            //{
+            //    new {Name = "Mushrooms", Count = 3},
+            //    new {Name = "Onions", Count = 1},
+            //    new {Name = "Olives", Count = 1},
+            //    new {Name = "Zucchini", Count = 1},
+            //    new {Name = "Pepperoni", Count = 2}
+            //};
+
+            //var json = pizza.ToGoogleDataTable()
+            //        .NewColumn(new Column(ColumnType.String, "Topping"), x => x.Name)
+            //        .NewColumn(new Column(ColumnType.Number, "Slices"), x => x.Count)
+            //        .Build()
+            //        .GetJson();
+
+            //return json;
+        }
+
+        public DataItem[] GetPlotData() 
+        { 
+            var row =  new DataItem[] {
+                new DataItem
+                {
+                    X = 2.75,
+                    Y = 50
+                },
+                new DataItem
+                {
+                    X = 5.5,
+                    Y = 60
+                },
+                new DataItem
+                {
+                    X = 7,
+                    Y = 70
+                },
+                new DataItem
+                {
+                    X = 9,
+                    Y = 80
+                },
+                new DataItem
+                {
+                    X = 11,
+                    Y = 90
+                },
+                new DataItem
+                {
+                    X = 12.5,
+                    Y = 100
+                },
+                new DataItem
+                {
+                    X = 15,
+                    Y = 110
+                }
+            };
+            return row;
+        }
+        //public DataTable getDataTable()
+        //{
+        //    // Create a DataTable.
+        //    // ... We add 4 columns, each with a Type.
+        //    DataTable table = new DataTable();
+        //    table.Columns.Add("Dosage", typeof(int));
+        //    table.Columns.Add("Drug", typeof(string));
+        //    table.Columns.Add("Diagnosis", typeof(string));
+        //    table.Columns.Add("Date", typeof(DateTime));
+
+        //    // Add rows.
+        //    table.Rows.Add(25, "Drug A", "Disease A", DateTime.Now);
+        //    table.Rows.Add(50, "Drug Z", "Problem Z", DateTime.Now);
+        //    table.Rows.Add(10, "Drug Q", "Disorder Q", DateTime.Now);
+        //    table.Rows.Add(21, "Medicine A", "Diagnosis A", DateTime.Now);
+        //    return table;
+        //}
     }
 
-    public static class DBHelper 
+    //public static class DBHelper 
+    //{
+    //    public static DataTable ConvertToDataTable<T>(IList<T> data)
+    //    {
+    //        PropertyDescriptorCollection properties =
+    //           TypeDescriptor.GetProperties(typeof(T));
+    //        DataTable table = new DataTable();
+    //        foreach (PropertyDescriptor prop in properties)
+    //            table.Columns.Add(prop.Name, Nullable.GetUnderlyingType(prop.PropertyType) ?? prop.PropertyType);
+    //        foreach (T item in data)
+    //        {
+    //            DataRow row = table.NewRow();
+    //            foreach (PropertyDescriptor prop in properties)
+    //                row[prop.Name] = prop.GetValue(item) ?? DBNull.Value;
+    //            table.Rows.Add(row);
+    //        }
+    //        return table;
+    //    }
+    //}
+
+    public class Chart
     {
-        public static DataTable ConvertToDataTable<T>(IList<T> data)
-        {
-            PropertyDescriptorCollection properties =
-               TypeDescriptor.GetProperties(typeof(T));
-            DataTable table = new DataTable();
-            foreach (PropertyDescriptor prop in properties)
-                table.Columns.Add(prop.Name, Nullable.GetUnderlyingType(prop.PropertyType) ?? prop.PropertyType);
-            foreach (T item in data)
-            {
-                DataRow row = table.NewRow();
-                foreach (PropertyDescriptor prop in properties)
-                    row[prop.Name] = prop.GetValue(item) ?? DBNull.Value;
-                table.Rows.Add(row);
-            }
-            return table;
-        }
+        //public Chart(object[] newCols, object[] newRows)
+        //{
+        //    cols = newCols;
+        //    rows = newRows;
+        //}
+
+        public object[] cols { get; set; }
+        public object[] rows { get; set; }
+    }
+
+    public class DataItem
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
     }
 }
 
--- Blazor.Charts.ChartJs.JsInterOp/wwwroot/js/chartjs.js
+++ Blazor.Charts.ChartJs.JsInterOp/wwwroot/js/chartjs.js
@@ -21,27 +21,74 @@
 //};
 
 window.drawChart = (data) => {
-    google.charts.load("current", { "packages": ["corechart"] });
-    google.charts.setOnLoadCallback(drawChart);
+    google.charts.load('current', { 'packages': ['corechart'] });
+    google.charts.setOnLoadCallback(drawGoogleChart);
+    function drawGoogleChart(data) {
+        //var jsonData = $.ajax({
+        //    url: "/Index?handler=ChartData",
+        //    dataType: "json",
+        //    async: false
+        //}).responseText;
+        //var jsondata = new google.visualization.DataTable(data);
+        //var jsondata = new google.visualization.DataTable(data);
 
-    function drawChart() {
-
-        var data = google.visualization.arrayToDataTable([
-            ['Year', 'Sales', 'Expenses'],
-            ['2004', 1000, 400],
-            ['2005', 1170, 460],
-            ['2006', 660, 1120],
-            ['2007', 1030, 540]
-        ]);
+            //var jsondata = google.visualization.arrayToDataTable([
+            //    ['Task', 'Hours per Day'],
+            //    ['Work', 11],
+            //    ['Eat', 2],
+            //    ['Commute', 2],
+            //    ['Watch TV', 2],
+            //    ['Sleep', 7]
+            //]);
+        var jsondata = new google.visualization.DataTable(
+            {
+                cols: [{ id: 'task', label: 'Employee Name', type: 'string' },
+                { id: 'hours', label: 'Hours', type: 'number' }],
+                rows: [{ c: [{ v: 'Mike' }, { v: 4 }] },
+                { c: [{ v: 'Bob' }, { v: 5 }] },
+                { c: [{ v: 'Alice' }, { v: 7 }] },
+                { c: [{ v: 'Frank' }, { v: 11 }] },
+                { c: [{ v: 'Floyd' }, { v: 13 }] },
+                { c: [{ v: 'Fritz' }, { v: 6 }] }
+                ]
+            }
+        )
 
         var options = {
-            title: 'Company Performance',
-            curveType: 'function',
-            legend: { position: 'bottom' }
+            title: 'My Daily Activities',
+            is3D: true,
         };
 
+        var chart = new google.visualization.PieChart(document.getElementById("piechart"));
+        chart.draw(jsondata, options);
         var chart = new google.visualization.LineChart(document.getElementById("curve_chart"));
-
-        chart.draw(data, options);
+        chart.draw(jsondata, options);
     }
+    //google.charts.load("current", { "packages": ["corechart"] });
+    //google.charts.setOnLoadCallback(drawChart);
+
+    //function drawChart(data) {
+
+    //    var data = google.visualization.arrayToDataTable([
+    //        [{ label: 'Year', id: 'year' },
+    //        { label: 'Sales', id: 'Sales', type: 'number' }, // Use object notation to explicitly specify the data type.
+    //        { label: 'Expenses', id: 'Expenses', type: 'number' }],
+    //        ['2014', 1000, 400],
+    //        ['2015', 1170, 460],
+    //        ['2016', 660, 1120],
+    //        ['2017', 1030, 540]]);
+
+    //    //var data = google.visualization.arrayToDataTable(data, false);
+    //    //var data = google.visualization.arrayToDataTable(data);
+
+    //    var options = {
+    //        title: 'Company Performance',
+    //        curveType: 'function',
+    //        legend: { position: 'bottom' }
+    //    };
+
+    //    var chart = new google.visualization.LineChart(document.getElementById("curve_chart"));
+
+    //    chart.draw(data, options);
+    //}
 };
