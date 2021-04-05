d68f3434f33693867f39f219c379b52801f1430d
rdw100 <ryandustinwright@hotmail.com>
2021-04-05 09:27:55 -0400

    Added datatable test methods from AnthroCloud.

--- Blazor.Charts.ChartJs/Components/LineChart.razor.cs
+++ Blazor.Charts.ChartJs/Components/LineChart.razor.cs
@@ -2,6 +2,7 @@
 using Microsoft.JSInterop;
 using System.Threading.Tasks;
 using Blazor.Charts.ChartJs.Entities;
+using System.Text.Json;
 
 namespace Blazor.Charts.ChartJs.Components
 {
@@ -77,6 +78,76 @@ namespace Blazor.Charts.ChartJs.Components
             {
                 X = 15,
                 Y = 110
+            }
+        };
+
+        readonly DataItem[] myScatterData = new DataItem[] {
+            new DataItem
+            {
+                Y = 2.75,
+                X = 50
+            },
+            new DataItem
+            {
+                Y = 5.5,
+                X = 60
+            },
+            new DataItem
+            {
+                Y = 7,
+                X = 70
+            },
+            new DataItem
+            {
+                Y = 9,
+                X = 80
+            },
+            new DataItem
+            {
+                Y = 11,
+                X = 90
+            },
+            new DataItem
+            {
+                Y = 12.5,
+                X = 100
+            },
+            new DataItem
+            {
+                Y = 15,
+                X = 110
+            }
+        };
+
+        readonly DataItem[] myScatterPlotData = new DataItem[] {
+            new DataItem
+            {
+                Y = 9.5,
+                X = 80
+            }
+        };
+
+        readonly WflDataItem[] myLineData = new WflDataItem[] {
+            new WflDataItem
+            {
+                //Sex = 1,
+                LengthInCm = 73.0,
+                Score = 9,
+                //L = -0.3521,
+                //M = 9.0865,
+                //S = 0.08269,
+                //Sd3neg = 7.200,
+                //Sd2neg = 7.700,
+                //Sd1neg = 8.400,
+                //Sd0 = 9.100,
+                //Sd1 = 9.900,
+                //Sd2 = 10.800,
+                //Sd3 = 11.800,
+                P3 = 7.800,
+                P15 = 8.400,
+                P50 = 9.100,
+                P85 = 9.900,
+                P97 = 10.700
             }
         };
 
@@ -158,11 +229,101 @@ namespace Blazor.Charts.ChartJs.Components
             // Inject the IJSRuntime abstraction into a class (.cs)
             await JSRuntime.InvokeVoidAsync("setup", Id, config);
         }
+
+        public string LoadChartData()
+        {
+            var myLabels = new object[]
+            {
+                new { label = "lengthincm", type = "number"},
+                new { label = "Score", type = "number"},
+                new { label = "97th", type = "number"},
+                new { label = "85th", type = "number"},
+                new { label = "50th", type = "number"},
+                new { label = "15th", type = "number"},
+                new { label = "3rd", type = "number"}
+            };
+
+            var myData = new object[]
+            {
+                new {
+                    LengthInCm = 73.0,
+                    Score = 9,
+                    P3 = 7.800,
+                    P15 = 8.400,
+                    P50 = 9.100,
+                    P85 = 9.900,
+                    P97 = 10.700
+                }
+            };
+
+            string result = string.Concat(myLabels, myData);
+
+            return result;
+        }
+
+        public string GetChartData()
+        {
+            var chart = new ChartDataTable
+            {
+                cols = new object[]
+                {
+                    new { id = "year", label = "Year", type = "string" },
+                    new { id = "sales", label = "Sales", type = "number" },
+                    new { id = "expenses", label = "Expenses", type = "number" }
+                },
+                rows = new object[]
+                {
+                    new { c = new object[] { new { v = "2014" }, new { v = 1000 }, new { v = 400 } } },
+                    new { c = new object[] { new { v = "2015" }, new { v = 1170 }, new { v = 460 } } },
+                    new { c = new object[] { new { v = "2016" }, new { v = 660 }, new { v = 1120 } } },
+                    new { c = new object[] { new { v = "2017" }, new { v = 1030 }, new { v = 540 } } }
+                }
+            };
+
+            string jsonString = JsonSerializer.Serialize(chart);
+
+            return jsonString;
+        }
+
     }
 
     class DataItem
     {
         public double X { get; set; }
         public double Y { get; set; }
+    }
+
+    public class WflDataItem
+    {
+        public int Sex { get; set; }
+        public double LengthInCm { get; set; }
+        public double L { get; set; }
+        public double M { get; set; }
+        public double S { get; set; }
+        public double Sd3neg { get; set; }
+        public double Sd2neg { get; set; }
+        public double Sd1neg { get; set; }
+        public double Sd0 { get; set; }
+        public double Sd1 { get; set; }
+        public double Sd2 { get; set; }
+        public double Sd3 { get; set; }
+        public double P3 { get; set; }
+        public double P15 { get; set; }
+        public double P50 { get; set; }
+        public double P85 { get; set; }
+        public double P97 { get; set; }
+        public double Score { get; set; }
+    }
+
+    public class DataLabels
+    {
+        public string Label { get; set; }
+        public string Type { get; set; }
+    }
+
+    public class ChartDataTable
+    {
+        public object[] cols { get; set; }
+        public object[] rows { get; set; }
     }
 }
